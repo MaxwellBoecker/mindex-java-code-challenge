3939b7b3271572e50500c3ca1b78ab05f4792017
MaxwellBoecker <boecker.maxwell@gmail.com>
2023-06-28 19:21:01 -0700

    refactor reporting structure

--- src/main/java/com/mindex/challenge/dao/impl/ReportingStructureRepositoryImpl.java
+++ src/main/java/com/mindex/challenge/dao/impl/ReportingStructureRepositoryImpl.java
@@ -11,56 +11,74 @@ import com.mindex.challenge.dao.ReportingStructureRepository;
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
 import org.springframework.stereotype.Repository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Repository
 public class ReportingStructureRepositoryImpl implements ReportingStructureRepository {
-    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureRepositoryImpl.class);
+    private int numberOfReports;
     
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    private int numberOfReports;
-
-    @Override
     public ReportingStructure findByEmployeeId(String employeeId) {
-        ReportingStructure reportingStructure = new ReportingStructure();
+        ReportingStructure result = new ReportingStructure();
+        result = addEmployeeToResult(employeeId, result);
+
+        findAllDirectReports(result.getEmployee().getDirectReports());       
+        result.setNumberOfReports(numberOfReports);
+
+        cleanup();
+        return result;
+    }
+
+    private ReportingStructure addEmployeeToResult(String employeeId, ReportingStructure reportingStructure){
         Employee employee = employeeRepository.findByEmployeeId(employeeId);
         reportingStructure.setEmployee(employee);
-        
-        findAllDirectReports(employee.getDirectReports());
-        
-        reportingStructure.setNumberOfReports(numberOfReports);
-        numberOfReports = 0;
         return reportingStructure;
     }
 
-    // This recursive function to expand the direct reports seemed funny at first. but the reason why I went for it was 
-    // because adding the DbRef and Id annotations on the Employee class brought back the full info for all nested employees
-    // and I though maybe this data was too much for what we needed. We want the current employee and the number of reports, don't need all the other stuff as well
-
-    // signature should be Employee, not List
+    // Design Decision: Implementing the recursive functions below to expand the direct reports seemed funny at first. But the reason why I went for it was 
+    // because adding the DbRef and Id annotations on the Employee class, so that I could get every employee with one query, brought back the full info for all nested employees.
+    // I thought maybe this data could be too much for what we needed to return to the end user. 
+    // We want the current employee and the number of reports, don't need all the other full employees' details as well
     private void findAllDirectReports(List<Employee> reports) {
         if(reports == null) {
             return;
         }
+        processReports(reports);
+    }
+
+    private void processReports(List<Employee> reports) {
         for (Iterator<Employee> directReports = reports.iterator(); directReports.hasNext();){
-            Employee currentEmployee = directReports.next();
-            this.numberOfReports += 1;
-            LOG.info("processing employeeId");
+            addDirectReport();
+            Employee currentEmployee = getCurrentEmployee(directReports);
 
-            Employee fullEmployeeInfo = expandEmployeeInfo(currentEmployee);
-
-            List<Employee> directEmployeeReports = fullEmployeeInfo.getDirectReports();
-            if(directEmployeeReports != null && !fullEmployeeInfo.getDirectReports().isEmpty()) {
-                findAllDirectReports(fullEmployeeInfo.getDirectReports());
+            if(hasDirectReports(currentEmployee)) {
+                findAllDirectReports(currentEmployee.getDirectReports());
             }
         }
     }
 
+    public void addDirectReport(){
+        this.numberOfReports += 1;
+    }
+
+    private Employee getCurrentEmployee(Iterator<Employee> directReports) {
+        Employee currentEmployee = directReports.next();
+        return expandEmployeeInfo(currentEmployee);
+    }
+    
+    // necessary to expand info because Employees returned in the directReports field from a Employee.findByEmployeeId() call only have employeeId enumerated
     private Employee expandEmployeeInfo(Employee employee) {
         return employeeRepository.findByEmployeeId(employee.getEmployeeId());
+    }
+
+    private boolean hasDirectReports(Employee employee) {
+        List<Employee> currentEmployeeDirectReports = employee.getDirectReports();
+        return currentEmployeeDirectReports != null && !currentEmployeeDirectReports.isEmpty();
+    }
+
+    private void cleanup(){
+        this.numberOfReports = 0;
     }
 
 }
